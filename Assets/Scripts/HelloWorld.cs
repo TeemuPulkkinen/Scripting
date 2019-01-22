010dc69889ddd3fe2fb0b04216d6a8746b90c5f7
TeemuPulkkinen <pulkkinen.teemu@gmail.com>
2019-01-22 11:19:19 +0200

    Second commit
    Tiedostojen lataus

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //public variable here
+    public string myGameIdea;
+
+    void Start()
+    {
+        print("Hello World");
+        //execute new method
+        PrintGame();
+    }
+
+    //create method here
+    void PrintGame()
+    {
+        print(myGameIdea);
+    }
+}
